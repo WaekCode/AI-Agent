f36b86c6c543178e2599497630ed5a1773a0f293
weak2055 <nati275a@gmail.com>
2025-06-22 21:32:23 +0200

    testing commit just removed  import time to see something didnt touch the code

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 from functions.run_python import run_python_file
 from functions.write_file import write_file
-import time
+
 
 
 # Load variables from .env file
